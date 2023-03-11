398e8d0a49bc0db6c063450252e61e614fc2d3c2
Arya <aryashirgaonkar42@gmail.com>
2023-03-11 12:55:43 +0530

    Added home page

---
+++ lib/Jobs.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Jobs extends StatefulWidget {
+  const Jobs({Key? key}) : super(key: key);
+
+  @override
+  State<Jobs> createState() => _JobsState();
+}
+
+class _JobsState extends State<Jobs> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Jobs"),
+      ),
+    );
+  }
+}
