83b9ad3c42678a76186f6631e5160f8ea103e6c6
Vatsal Shah <vsshah2003@gmail.com>
2023-03-11 12:38:16 +0530

    Added constant Colors

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ResourceColors {
+  static const Color slightWhite = Color(0xffecf2ff);
+  static const Color primaryColor = Color(0xff3e54ac);
+  static const Color secondaryColor = Color(0xff655dbb);
+  static const Color tertiaryColor = Color(0xffbface2);
+}
